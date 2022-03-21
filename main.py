65b14aee42d55df3553920eaeebecf73399c21f0
GrechTech <thomas@grechtech.co.uk>
2022-03-21 11:22:56 +0000

    Image embed only check

--- main.py
+++ main.py
@@ -181,13 +181,14 @@ async def on_message(message):
                 return
         
         # Autosnail
-        if not message.embeds:
-            urls = FindURL(message.content)
-            # Check message for url
-            if len(urls) > 0:
-                for url in urls:
-                    snail = False
-                    clean_url = url.rstrip().lower()
+        urls = FindURL(message.content)
+        # Check message for url
+        if len(urls) > 0:
+            for url in urls:
+                snail = False
+                clean_url = url.rstrip().lower()
+                ToCheck = ['.png', '.gif', '.jpg','.jpeg', 'discordapp','tenor']
+                if not [ele for ele in ToCheck if(ele in clean_url)]:
                     if clean_url.contains("youtube.com/watch?v="):
                         clean_url = clean_url.replace("youtube.com/watch?v=","youtu.be/")
                     
