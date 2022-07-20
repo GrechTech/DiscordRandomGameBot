5ec20bdc617d858f243c38fe6568bdac80a04049
tgrech1997 <45184353+tgrech1997@users.noreply.github.com>
2022-07-20 14:43:43 +0100

    Cleanup

--- banword.py
+++ banword.py
@@ -6,7 +6,6 @@ BAN_LIST = os.path.join(DIR_PATH,"Config","ban_list.txt")
 
 def _getBannedWords():
     newlines = []
-    # Check each line of file
     
     with open(BAN_LIST, "r") as file:
         for line in file:
@@ -25,7 +24,5 @@ def _words_in_string(word_list,a_string):
     return False
 
 async def CheckForWords(message, bot):
-    msg = message.content.lower()
-    if _words_in_string(BannedWords, msg):
-        emoji = discord.utils.get(bot.emojis, name="greenwithit")
-        await message.add_reaction(emoji)
+    if _words_in_string(BannedWords, message.content.lower()):
+        await message.add_reaction(discord.utils.get(bot.emojis, name="greenwithit"))
